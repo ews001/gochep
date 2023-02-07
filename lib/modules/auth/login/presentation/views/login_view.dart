471f3c052a877e9aeda49cf51ba0b2cfda7f1e0b
ewsmexico <admin@easywayservices.app>
2023-02-07 13:12:06 -0800

    change route to forgot password view.

--- lib/modules/auth/login/presentation/views/login_view.dart
+++ lib/modules/auth/login/presentation/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gochep/modules/auth/forgot_password/presentation/views/forgot_password_view.dart';
 import 'package:gochep/modules/auth/login/bloc/login_bloc.dart';
 import 'package:gochep/modules/auth/login/presentation/views/widets/password_input_widget.dart';
 import 'package:gochep/modules/auth/login/presentation/views/widets/sign_in_with_social_providers.dart';
@@ -21,58 +22,55 @@ class LoginView extends HookWidget {
     return Scaffold(
         body: BlocProvider(
       create: (_) => bloc,
-      child: BlocConsumer<LoginBloc, LoginState>(
+      child: BlocListener<LoginBloc, LoginState>(
         listener: (context, state) {
           switch (state.runtimeType) {
             case LoginStateIsInForgotPassword:
               Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const SplashView(),
+                builder: (context) => const ForgotPasswordView(),
               ));
               break;
             default:
           }
         },
-        builder: (context, state) {
-          return SingleChildScrollView(
-            child: FullContainer(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  heightSpaceX2,
-                  // heightSpaceX2,
-                  Padding(
-                    padding: fixPaddingX2,
-                    child: Column(
-                      children: [
-                        header(),
-                        heightSpaceX2,
-                        heightSpaceX2,
-                        EmailInputWidget(
-                          stream: bloc.email,
-                          onChangeEvent: bloc.changeEmail,
-                        ),
-                        heightSpace,
-                        PasswordInputWidget(
-                          stream: bloc.password,
-                          onChangeEvent: bloc.changePassword,
-                        ),
-                        heightSpace,
-                        BtnLoginWidget(
-                          stream: bloc.submitValid,
-                        ),
-                        const ForgotPasswordsBtnWidget()
-                      ],
-                    ),
+        child: SingleChildScrollView(
+          child: FullContainer(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                heightSpaceX2,
+                Padding(
+                  padding: fixPaddingX2,
+                  child: Column(
+                    children: [
+                      header(),
+                      heightSpaceX2,
+                      heightSpaceX2,
+                      EmailInputWidget(
+                        stream: bloc.email,
+                        onChangeEvent: bloc.changeEmail,
+                      ),
+                      heightSpace,
+                      PasswordInputWidget(
+                        stream: bloc.password,
+                        onChangeEvent: bloc.changePassword,
+                      ),
+                      heightSpace,
+                      BtnLoginWidget(
+                        stream: bloc.submitValid,
+                      ),
+                      const ForgotPasswordsBtnWidget()
+                    ],
                   ),
-                  heightSpace,
-                  const LoginFormWithSocialProviders(),
-                  const BtnSignUpWidget()
-                ],
-              ),
+                ),
+                heightSpace,
+                const LoginFormWithSocialProviders(),
+                const BtnSignUpWidget()
+              ],
             ),
-          );
-        },
+          ),
+        ),
       ),
     ));
   }
