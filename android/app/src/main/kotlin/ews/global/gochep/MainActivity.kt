2e300cc727c9ed7e0256002ef998a64fba2fb87d
ewsmexico <admin@easywayservices.app>
2023-02-07 16:13:30 -0800

    add firebase services.

--- android/app/src/main/kotlin/ews/global/gochep/MainActivity.kt
+++ android/app/src/main/kotlin/ews/global/gochep/MainActivity.kt
@@ -1,6 +1,20 @@
 package ews.global.gochep
 
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+   
+    override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(/*context=*/ this);
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance()
+        )
+        super.onCreate(savedInstanceState)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gochep/generated/l10n.dart';
@@ -5,35 +6,36 @@ import 'package:gochep/modules/auth/login/presentation/views/login_view.dart';
 import 'package:gochep/settings/size_config.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  runApp(
-    LayoutBuilder(builder: (context, constraints) {
-      return OrientationBuilder(
-        builder: (context, orientation) {
-          SizeConfig().init(constraints, orientation);
-          return MaterialApp(
-            localizationsDelegates: const [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-              S.delegate
-            ],
-            locale: const Locale('en'),
-            supportedLocales: S.delegate.supportedLocales,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            debugShowCheckedModeBanner: true,
-            home: const LoginView(),
-          );
-        },
-      );
-    }),
-  );
+  await Firebase.initializeApp().then((value) async => run());
 }
+
+void run() => runApp(
+      LayoutBuilder(builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            SizeConfig().init(constraints, orientation);
+            return MaterialApp(
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+                S.delegate
+              ],
+              locale: const Locale('en'),
+              supportedLocales: S.delegate.supportedLocales,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              debugShowCheckedModeBanner: true,
+              home: const LoginView(),
+            );
+          },
+        );
+      }),
+    );
